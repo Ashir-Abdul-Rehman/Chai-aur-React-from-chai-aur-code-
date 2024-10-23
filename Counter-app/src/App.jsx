d260d57c50cf47e8cc6fce31153f1b760c6a525f
Ashir Abdul Rehman <ashirabdulrehman@gmail.com>
2024-10-23 17:24:01 +0500

    add project Counter-app

---
+++ Counter-app/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+    let [value, setvalue] = useState(0)
+    function addvalue(){
+      setvalue(value + 1)
+  }
+
+  function removevalue(){
+    setvalue(value - 1)
+  }
+
+  function resetvalue(){
+    setvalue(0)
+  }
+
+  return (
+    <>
+      <h1>Counter app with react</h1>
+      <br />
+      <br />
+      <h2>Count: {value}</h2>
+      <br />
+      <button onClick={addvalue}>Increment</button>
+      <br />
+      <br />
+      <button onClick={removevalue}>Decrement</button>
+      <br />
+      <br />
+      <button onClick={resetvalue}>Decrement</button>
+    </>
+  )
+}
+
+export default App
