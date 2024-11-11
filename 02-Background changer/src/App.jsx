5952165cf23882bc58c2a1839db75ab064ea0b5a
Ashir Abdul Rehman <ashirabdulrehman@gmail.com>
2024-11-11 20:50:45 +0500

    Completed project '01 bg-changer' using 'usestate' and 'useeffect' hooks

--- 02-Background changer/src/App.jsx
+++ 02-Background changer/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('#ffffff');
+const green =()=>{   
+  setColor('#166534')
+} 
+const red =()=>{ 
+  setColor('#991b1b')
+}
 
+const blue =()=>{   
+  setColor('#1e40af')
+}
+
+
+useEffect(() => {
+  document.body.style.backgroundColor = color; // Change body background color
+}, [color]); 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='flex absolute bottom-12  flex-wrap h-16 w-[60%] border-none rounded bg-[#e3e3e3] text-white justify-evenly  items-center '>
+        <button onClick={red}  className='bg-red-800  h-9 border-2 rounded-full w-16'>red</button>
+        <button onClick={blue} className='bg-blue-800 h-9 border-2 rounded-full w-16'>blue</button>
+        <button onClick={green} className='bg-green-800 h-9 border-2 w-16 rounded-full'>green</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
