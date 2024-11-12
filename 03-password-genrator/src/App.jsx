d6654514181d0a162bf16b1e06feb645ee8551f4
Ashir Abdul Rehman <ashirabdulrehman@gmail.com>
2024-11-12 22:38:43 +0500

    Completed frontend of  project 03-password-genrator

--- 03-password-genrator/src/App.jsx
+++ 03-password-genrator/src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <h1 className='text-3xl font-bold underline text-center ' >Password Genrator</h1>
+    <div className='flex justify-center mt-6'>
+    <div className="div mt-6 flex items-center justify-center bg-[#3d3d3d] w-[600px] rounded-sm flex-wrap">
+      <h3 className='h-12 border-none w-[486.389px] bg-[#000000a6] pt-3 rounded-l-md '>Password</h3>
+      <button className='bg-black text-white h-12 w-auto border-none rounded-r-md '><svg className='w-12 h-12 p-1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M7 4V2H17V4H20.0066C20.5552 4 21 4.44495 21 4.9934V21.0066C21 21.5552 20.5551 22 20.0066 22H3.9934C3.44476 22 3 21.5551 3 21.0066V4.9934C3 4.44476 3.44495 4 3.9934 4H7ZM7 6H5V20H19V6H17V8H7V6ZM9 4V6H15V4H9Z"></path></svg></button>
+      <div className='mt-8 flex justify-center flex-wrap'>
+      <input type="range" name="" id="" className='mr-2' />
+      <p>{"length ( )"}</p>
+      <input type="checkbox" name="" id="num" className='ml-2' /> <label htmlFor="num" className='ml-2'>Name</label>
+      <input type="checkbox" name="" id="char" className='ml-2' /> <label htmlFor="char" className='ml-2'>Chracters</label>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
+    </div>
     </>
   )
 }
